f3317b873678584b537b6268161d321b485217df
Lucifer <wdtxzy@126.com>
2019-03-20 23:34:40 +0800

    1.find duplicate num in array 2.find two lost mum in array

---
+++ Arithmetic/Java/FindDuplicateNumInArray.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ * @author : wangdi
+ * @time : creat in 2019/3/20 22:10
+ * 给定一个数组，如何搜索重复元素？
+ */
+public class FindDuplicateNumInArray {
+
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        Random random = new Random();
+        for(int i=0;i<100;i++){
+            int tmp = random.nextInt(99)+1;
+            arr[i] = tmp;
+        }
+        FindDuplicateNumInArray findDuplicateNumInArray = new FindDuplicateNumInArray();
+        findDuplicateNumInArray.findDuplicateNum(arr);
+    }
+
+    private void findDuplicateNum(int[] array){
+        HashMap map = new HashMap();
+        int i=0;
+        while (!map.containsKey(array[i])){
+            map.put(array[i],0);
+            i++;
+        }
+        System.out.println(i);
+        System.out.println(array[i]);
+    }
+}
---
+++ Arithmetic/Java/FindMaxAndMinInArray.java
@@ -0,0 +1,14 @@
+/**
+ * @author : wangdi
+ * @time : creat in 2019/3/20 23:20
+ */
+public class FindMaxAndMinInArray {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void findMaxAndMin(int[] arr){
+
+    }
+}
